0a0cd13473b1bf507abed126c065058db4ef4571
bluekani <24561326+bluekani@users.noreply.github.com>
2026-07-02 11:52:24 +0900

    fix: restore build scripts to master branch

--- scripts/build-native.mjs
+++ scripts/build-native.mjs
@@ -19,6 +19,7 @@ for (let dir of ['app', 'tabby-core', 'tabby-local', 'tabby-ssh', 'tabby-termina
         buildPath: path.resolve(__dirname, '../' + dir),
         electronVersion: vars.electronVersion,
         arch: process.env.ARCH,
+        force: true,
     })
     build.catch(e => {
         console.error(e)
