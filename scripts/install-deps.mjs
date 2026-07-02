703281a9fbcaed8db3f03602c7fe597746dda5cc
bluekani <24561326+bluekani@users.noreply.github.com>
2026-07-02 09:09:57 +0900

    feat: add timing information to install-deps.mjs

--- scripts/install-deps.mjs
+++ scripts/install-deps.mjs
@@ -3,27 +3,41 @@ import sh from 'shelljs'
 import * as vars from './vars.mjs'
 import log from 'npmlog'
 
+function ms(n) {
+    return n < 1000 ? `${n}ms` : `${(n / 1000).toFixed(1)}s`
+}
+
+const overallStart = Date.now()
+
 log.info('patch')
+const patchStart = Date.now()
 sh.exec(`yarn patch-package`, { fatal: true })
+console.info(`patch-package ${ms(Date.now() - patchStart)}`)
 
 log.info('deps', 'app')
-
+const appStart = Date.now()
 sh.cd('app')
 sh.exec(`yarn install --force --network-timeout 1000000`, { fatal: true })
 // Some native packages might fail to build before patch-package gets a chance to run via postinstall
 sh.exec(`yarn postinstall`, { fatal: false })
 sh.cd('..')
+console.info(`[app] ${ms(Date.now() - appStart)}`)
 
+log.info('deps', 'web')
+const webStart = Date.now()
 sh.cd('web')
 sh.exec(`yarn install --force --network-timeout 1000000`, { fatal: true })
 sh.exec(`yarn patch-package`, { fatal: true })
 sh.cd('..')
+console.info(`[web] ${ms(Date.now() - webStart)}`)
 
 vars.allPackages.forEach(plugin => {
     log.info('deps', plugin)
+    const pluginStart = Date.now()
     sh.cd(plugin)
     sh.exec(`yarn install --force --network-timeout 1000000`, { fatal: true })
     sh.cd('..')
+    console.info(`[${plugin}] ${ms(Date.now() - pluginStart)}`)
 })
 
 if (['darwin', 'linux'].includes(process.platform)) {
@@ -33,3 +47,5 @@ if (['darwin', 'linux'].includes(process.platform)) {
     }
     sh.cd('..')
 }
+
+console.info(`\nTotal: ${ms(Date.now() - overallStart)}`)
